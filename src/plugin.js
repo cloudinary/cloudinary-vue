b8d77593ae80133a69b96e297b258cc4e93cab51
Artur Kulig <arturkulig@gmail.com>
2019-05-29 15:54:26 +0200

    make so components can be a component identifiers
    component objects can now be a component identifier
    just as well as a string that contains a component name

--- src/plugin.js
+++ src/plugin.js
@@ -8,6 +8,17 @@ import CldTransformation from "./components/CldTransformation";
 import CldVideo from "./components/CldVideo";
 import CldPicture from "./components/CldPicture";
 import CldSource from "./components/CldSource";
+import { find } from "./utils";
+
+const allComponents = [
+  CldContext,
+  CldImage,
+  CldPoster,
+  CldTransformation,
+  CldVideo,
+  CldPicture,
+  CldSource
+];
 
 export function install(Vue, options) {
   if (Vue.CldInstalled) {
@@ -17,15 +28,7 @@ export function install(Vue, options) {
 
   options = options || {};
 
-  [
-    CldContext,
-    CldImage,
-    CldPoster,
-    CldTransformation,
-    CldVideo,
-    CldPicture,
-    CldSource
-  ].forEach(component => {
+  allComponents.forEach(component => {
     const userComponentName = getUserComponentName(
       options.components,
       component.name
@@ -48,23 +51,42 @@ function getUserComponentName(components, name) {
   }
 
   // { components: ['CldImage'] }
+  // and
+  // { components: [CldImage] }
   if (Array.isArray(components)) {
-    return components.indexOf(name) >= 0 ? name : null;
+    const entry = find(
+      components,
+      component =>
+        (typeof component === "string" && component === name) ||
+        (typeof component === "object" &&
+          component != null &&
+          component.name === name)
+    );
+    if (entry == null) {
+      return undefined;
+    }
+    if (typeof entry === "string") {
+      return entry;
+    }
+    return entry.name;
   }
   // { components: { CldImage: true } }
   if (typeof components[name] === "boolean") {
-    return components[name] === true ? name : null;
+    return components[name] === true ? name : undefined;
   }
   // { components: { CldImage: 'CloudinaryImage' } }
   if (typeof components[name] === "string") {
     return components[name];
   }
   // { components: { CloudinaryImage: 'CldImage' } }
-  const keys = Object.keys(components);
-  const values = keys.map(key => components[key]);
-  if (values.indexOf(name) >= 0) {
-    return keys[values.indexOf(name)];
-  }
-
-  return null;
+  // and
+  // { components: { CloudinaryImage: CldImage } }
+  return find(
+    Object.keys(components),
+    k =>
+      (typeof components[k] === "string" && components[k] === name) ||
+      (typeof components[k] === "object" &&
+        components[k] != null &&
+        components[k].name === name)
+  );
 }

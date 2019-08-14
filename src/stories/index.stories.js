cdbffa3967ac44df3ace22fabdea6290fbc2c61e
Artur Kulig <arturkulig@gmail.com>
2019-08-14 17:33:54 +0300

    add storybook

---
+++ src/stories/index.stories.js
@@ -0,0 +1,16 @@
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+// import { linkTo } from "@storybook/addon-links";
+
+import CldImage from "../components/CldImage.vue";
+
+storiesOf("CldImage", module).add("simple", () => ({
+  components: { CldImage },
+  template: `
+      <cld-image 
+        cloudName="demo" 
+        publicId="small_dinosaur"
+        @click.native="action"
+      />`,
+  methods: { action: action("clicked") }
+}));

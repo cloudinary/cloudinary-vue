a8538df0e8fc886f3e3499104ec3ac4221e8d112
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2020-05-18 11:16:02 +0300

    Deprecate prop lazy and introduce loading="lazy" (#74)

--- tests/unit/img.spec.js
+++ tests/unit/img.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
 import CldImage from "../../src/components/CldImage/CldImage.vue";
 
 
@@ -56,5 +56,80 @@ describe("CldImage", () => {
       propsData: getImageProps({ accessibility:'colorblind'})
     }).find('img');
     expect(image.attributes("src")).toContain('e_assist_colorblind/face_top');
+  });
+
+  it('Loads an image with lazyload', () => {
+    let intersectCallback = null;
+    let nativeIntersectionObserver = global.IntersectionObserver;
+
+    // Mock InterscetionObserver
+    global.IntersectionObserver = class {
+      constructor(cb) {
+        // This is the callback the notifieis when an intersection occures
+        // We'll store it to use it later
+        intersectCallback = cb;
+      }
+      observe() {}
+    };
+
+    // Create an instance
+    const wrap = mount(CldImage, {
+      propsData: getImageProps({
+        loading:'lazy',
+      })
+    });
+
+    // Since we're lazy loading, we expect not to be visible
+    expect(wrap.vm.visible).toBe(false);
+
+    // Mock an intersection call, this should turn the component visible
+    intersectCallback([{
+      target: wrap.element,
+      isIntersecting: true
+    }]);
+
+    // We should now be visible
+    expect(wrap.vm.visible).toBe(true);
+
+    // restore
+    global.IntersectionObserver = nativeIntersectionObserver;
+  });
+
+  it ('Lazyloads with a placeholder', () => {
+    let nativeIntersectionObserver = global.IntersectionObserver;
+    let intersectCallback = null;
+
+    // Mock InterscetionObserver
+    global.IntersectionObserver = class {
+      constructor(cb) {
+        // This is the callback the notifieis when an intersection occures
+        // We'll store it to use it later
+        intersectCallback = cb;
+      }
+      observe() {}
+    };
+
+    // Mock InterscetionObserver
+    global.IntersectionObserver = class {
+      constructor(cb) {
+        // This is the callback the notifieis when an intersection occures
+        // We'll store it to use it later
+        intersectCallback = cb;
+      }
+      observe() {}
+    };
+
+    const wrap = mount(CldImage, {
+      propsData: getImageProps({
+        loading:'lazy',
+        placeholder: 'color'
+      })
+    });
+
+    expect(wrap.find('img').attributes("src")).toEqual(
+      `http://res.cloudinary.com/demo/image/upload/$nh_ih,$nw_iw,c_scale,q_1,w_1/c_scale,h_$nh,w_$nw/face_top`
+    );
+
+    global.IntersectionObserver = nativeIntersectionObserver;
   });
 });

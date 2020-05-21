58d65ce2f7a0e6e4306a9b1040aa04cd8e67baa1
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2020-05-21 21:29:09 +0300

    Uncomment a test

--- tests/unit/PlaceholderComponent/placeholder_blur.spec.js
+++ tests/unit/PlaceholderComponent/placeholder_blur.spec.js
@@ -27,26 +27,26 @@ describe("CldPlaceholder", () => {
     );
   });
 
-  // it("Should remove placeholder once CldImage is loaded", async () => {
-  //   let wrapper = mount(
-  //     {
-  //       template: `
-  //         <cld-image cloudName="demo" publicId="face_top">
-  //           <cld-placeholder type="blur"></cld-placeholder>
-  //         </cld-image>
-  //       `
-  //     },
-  //     {
-  //       components: {CldImage, CldPlaceholder}
-  //     }
-  //   );
-  //
-  //   let cldPlaceholder = wrapper.findAll("img").at(1);
-  //
-  //   await Vue.nextTick();
-  //
-  //   expect(cldPlaceholder.attributes("src")).toBe(
-  //     `http://res.cloudinary.com/demo/image/upload/e_blur:2000,f_auto,q_1/face_top`
-  //   );
-  // });
+  it("Should remove placeholder once CldImage is loaded", async () => {
+    let wrapper = mount(
+      {
+        template: `
+          <cld-image cloudName="demo" publicId="face_top">
+            <cld-placeholder type="blur"></cld-placeholder>
+          </cld-image>
+        `
+      },
+      {
+        components: {CldImage, CldPlaceholder}
+      }
+    );
+
+    let cldPlaceholder = wrapper.findAll("img").at(1);
+
+    await Vue.nextTick();
+
+    expect(cldPlaceholder.attributes("src")).toBe(
+      `http://res.cloudinary.com/demo/image/upload/e_blur:2000,f_auto,q_1/face_top`
+    );
+  });
 });

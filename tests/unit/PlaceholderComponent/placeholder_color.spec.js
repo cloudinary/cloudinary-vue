b90272204d2cf8bf0fe0829278563b0569e26402
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2020-05-24 12:51:30 +0300

    Add test for a single pixel placeholder

--- tests/unit/PlaceholderComponent/placeholder_color.spec.js
+++ tests/unit/PlaceholderComponent/placeholder_color.spec.js
@@ -4,7 +4,7 @@ import CldPlaceholder from "../../../src/components/CldPlaceholder/CldPlaceholde
 import Vue from "vue";
 
 describe("CldPlaceholder", () => {
-  it("Should remove placeholder once CldImage is loaded", async () => {
+  it("Should add a color placeholder", async () => {
     let wrapper = mount(
       {
         template: `
@@ -24,6 +24,32 @@ describe("CldPlaceholder", () => {
 
     expect(cldPlaceholder.attributes("src")).toBe(
       `http://res.cloudinary.com/demo/image/upload/$currWidth_w,$currHeight_h/ar_1,b_auto,c_pad,w_iw_div_2/c_crop,g_north_east,h_10,w_10/c_fill,h_$currHeight,w_$currWidth/f_auto,q_auto/face_top`
+    );
+  });
+
+  it("Should have a stretched single pixel when possible", async () => {
+    let wrapper = mount(
+      {
+        template: `
+          <cld-image cloudName="demo" publicId="face_top" width="100" height="200">
+            <cld-placeholder type="predominant-color"></cld-placeholder>
+          </cld-image>
+        `
+      },
+      {
+        components: {CldImage, CldPlaceholder}
+      }
+    );
+
+    let cldPlaceholder = wrapper.findAll("img").at(1);
+
+    await Vue.nextTick();
+
+
+    expect(cldPlaceholder.attributes().width).toBe('100');
+    expect(cldPlaceholder.attributes().height).toBe('200');
+    expect(cldPlaceholder.attributes("src")).toBe(
+      `http://res.cloudinary.com/demo/image/upload/ar_1,b_auto,c_pad,w_iw_div_2/c_crop,g_north_east,h_1,w_1/f_auto,q_auto/face_top`
     );
   });
 });

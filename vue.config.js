72b15eddcb6229818000b139a74c1f2f9f0b2cb1
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2020-08-20 13:30:14 +0300

    Add yarn serve to use as a local dev environment

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   configureWebpack: config => {
-    if (process.env.STYLEGUIDE) {
+    if (process.env.INTERNAL_DEPS) {
       return;
     }
     config.externals = [nodeExternals()];

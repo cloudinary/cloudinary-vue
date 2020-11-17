b15166296ba57dc54de95109120ce911e8d3d99f
Maya Shavin <maya@cloudinary.com>
2020-11-17 19:05:01 +0200

    create mixin for setting up cloudinary per comp

---
+++ src/mixins/setup.js
@@ -0,0 +1,32 @@
+import { Cloudinary } from 'cloudinary-core';
+/**
+ * To set up Cloudinary instance in component
+ */
+export const setup = {
+  data() {
+    return {
+      defaultConfigurations: {}
+    }
+  },
+  inject: {
+    contextConfiguration: {
+      default: {}
+    },
+    contextOptions: {
+      default: {}
+    },
+  },
+  methods: {
+    setup(extraConfigs) {      
+      if (!this.cloudinary) {
+        this.cloudinary = new Cloudinary(this.defaultConfigurations)
+      }
+
+      this.cloudinary.config(this.contextOptions)
+
+      if (extraConfigs) {
+        this.cloudinary.config(extraConfigs)
+      }
+    }
+  }
+}; 
